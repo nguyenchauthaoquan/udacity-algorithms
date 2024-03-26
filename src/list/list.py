3ace340ff8c0585e688c0c37b6bfc2d4cc81960a
Quan Nguyen <QuanNCT@outlook.com>
2024-03-27 00:39:55 +0700

    Implement tim sort

--- src/list/list.py
+++ src/list/list.py
@@ -1,6 +1,8 @@
 import math
 from random import randint
 
+TIM_SORT_MIN_SIZE = 32
+
 
 def rotate_list(list: []) -> []:
     if not list:
@@ -88,3 +90,72 @@ def meta_binary_search(list: [], value: int) -> int:
             index = new_index
 
     return index if list[index] == value else -1
+
+
+def insertion_sort(list: [], comparator=lambda x, y: x < y) -> []:
+    if len(list) > 1:
+        for i in range(1, len(list)):
+            key = list[i]
+
+            current_index = i - 1
+
+            while current_index >= 0 and comparator(key, list[current_index]):
+                list[current_index + 1] = list[current_index]
+                current_index -= 1
+            list[current_index + 1] = key
+        return list
+    else:
+        return []
+
+
+def merge_sort(list: [], comparator=lambda x, y: x < y) -> []:
+    if len(list) > 1:
+        mid_index = len(list) // 2
+        left_items = list[:mid_index]
+        right_items = list[mid_index:]
+
+        merge_sort(left_items, comparator)
+        merge_sort(right_items, comparator)
+
+        left_index = right_index = main_index = 0
+
+        while left_index < len(left_items) and right_index < len(right_items):
+            if comparator(left_items[left_index], right_items[right_index]):
+                list[main_index] = left_items[left_index]
+                left_index += 1
+            else:
+                list[main_index] = right_items[right_index]
+                right_index += 1
+            main_index += 1
+
+        while left_index < len(left_items):
+            list[main_index] = left_items[left_index]
+            left_index += 1
+            main_index += 1
+
+        while right_index < len(right_items):
+            list[main_index] = right_items[right_index]
+            right_index += 1
+            main_index += 1
+
+        return list
+    else:
+        return []
+
+
+def tim_sort(list: [], comparator=lambda x, y: x < y):
+    for i in range(0, len(list), TIM_SORT_MIN_SIZE):
+        insertion_sort(list[i: min((i + TIM_SORT_MIN_SIZE), len(list))], comparator)
+
+    size = TIM_SORT_MIN_SIZE
+
+    while size < len(list):
+        for left_index in range(0, len(list), 2 * size):
+            middle_index = left_index + size
+            right_index = min((left_index + 2 * size), len(list))
+            if middle_index < right_index:
+                merge_sort(list[left_index: right_index], comparator)
+
+        size *= 2
+
+    return list
