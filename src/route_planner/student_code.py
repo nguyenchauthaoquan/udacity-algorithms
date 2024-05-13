a26b81a517ac2e9a7f2c4eeca67e73ebeb59f9f5
Quan Nguyen <QuanNCT@outlook.com>
2024-05-13 22:26:00 +0700

    Finalize the project 4

--- src/route_planner/student_code.py
+++ src/route_planner/student_code.py
@@ -17,6 +17,7 @@ def shortest_path(graph, start, goal):
             get_paths(traversed_paths, start, goal)
 
         for node in graph.roads[current_node]:
+            # Calculate f(n) = g(n) + h(n); with h(n) is the weight between 2 vertices, g is the weight vertex
             h = calculate_h(graph.intersections[current_node], graph.intersections[node])
             f = g[current_node] + h
 
@@ -30,10 +31,23 @@ def shortest_path(graph, start, goal):
 
 
 def calculate_h(start, goal):
+    """
+    Calculate the h value between start and goal.
+    :param start: The start node
+    :param goal: The end node
+    :return: h value between start and goal
+    """
     return math.sqrt(((start[0] - goal[0]) ** 2) + ((start[1] - goal[1]) ** 2))
 
 
 def get_paths(traversed_paths, start, goal):
+    """
+    Gets the paths from start to goal from traversed paths
+    :param traversed_paths: The traversed paths from graph
+    :param start: The start node
+    :param goal: The goal node
+    :return: All paths from start to goal from traversed paths
+    """
     current_node = goal
     paths = [current_node]
 
